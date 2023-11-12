24467426ce786784258be799966e7377e6c23140
Ahmed Shafique <2021ee313@student.uet.edu.pk>
2023-11-12 14:29:45 +0500

    cost values are find

--- cost_function.py
+++ cost_function.py
@@ -1,19 +1,23 @@
 # defining cost function in this python program
 import numpy as np
 import matplotlib.pyplot as plt
-x = np.random.randint(100, 900, 250)
 c= 50000
-from actual_price import Prices
-def cost_function(x, c,  prices):
-    m =x.shape[0]
-    y = prices
-    cost_sum = 0
-    for i in range(m):
-        f_wb = m*x[i] + c
-        cost = (f_wb - y[i])**2
-        cost_sum = cost_sum + cost
-    total_cost = (1/(2*m) * cost_sum)
-   
-    return total_cost
+from actual_price import Prices, x
+m =x.shape[0]
+y = Prices
 
-print(cost_function(x, c, Prices))
+# loop 
+for i in range(m):
+    def cost_function(x, c,  prices): 
+        cost_sum = 0
+        for i in range(m):
+            f_wb = m*x[i] + c
+            cost = (f_wb - y[i])**2
+            cost_sum = cost_sum + cost
+            total_cost = (1/(2*m) * cost_sum)
+   
+        return total_cost
+
+
+cost_values= [cost_function(x,c,Prices)]
+
--- sample_dataset.py
+++ sample_dataset.py
@@ -1,8 +1,14 @@
-import numpy as np
-import matplotlib.pyplot as plt
+# import numpy as np
+# import matplotlib.pyplot as plt
 
-np.random.seed(24)
-x = np.random.randint(100, 900, 250)
-y= np.random.randint(50000, 100000, 250)
-plt.scatter(x,y)
-plt.show()
+# np.random.seed(24)
+# x = np.random.randint(100, 900, 250)
+# y= np.random.randint(50000, 100000, 250)
+# plt.scatter(x,y)
+# plt.show()
+hide = [3235, 3546, 5976, 1656, 1038]
+my_array = []
+for i in range(2):
+    my_array= i * hide
+
+print(my_array)
